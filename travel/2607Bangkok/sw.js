8a6794e4c3612a91ea4a4158cc978e85cad47c7c
xuzhiyuan1 <xuzhiyuan1@users.noreply.github.com>
2026-07-15 11:58:40 +0000

    travel: equal-height topbar controls, left accent border on all cards, show v1 countdown A alongside current B for comparison, B clip 15 chars

--- travel/2607Bangkok/sw.js
+++ travel/2607Bangkok/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "travel-bkk-v7";
+const CACHE = "travel-bkk-v8";
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./itinerary.html", "./data.js", "./durian.png"])));
   self.skipWaiting();
