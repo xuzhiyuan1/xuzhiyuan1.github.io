ff4cfba02b10dfb1fc9ab2f74c7c90a3a268bf06
xuzhiyuan1 <xuzhiyuan1@users.noreply.github.com>
2026-08-11 03:25:26 +0000

    Switch 2610Paris to an orange Eiffel-only design

--- travel/2610Paris/sw.js
+++ travel/2610Paris/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "travel-paris-v1";
+const CACHE = "travel-paris-orange-v1";
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll([
     "./",
@@ -7,7 +7,6 @@ self.addEventListener("install", e => {
     "./ui/style.css",
     "./ui/app.js",
     "./ui/eiffel.svg",
-    "./ui/city-mountain-sea.svg",
     "./ui/prince.png",
     "./data/site.json",
     "./data/trip.json",
